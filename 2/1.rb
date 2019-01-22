e7f2fe79e4e055131143b939fadc3e3a3766da91
Dmitri Tchemski <tchemski@gmail.com>
2019-01-22 15:51:25 +0300

    Solved 2/1.rb

---
+++ 2/1.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby -w
+
+# Сделать хеш, содеращий месяцы и количество дней в месяце. В цикле выводить
+# те месяцы, у которых количество дней ровно 30
+
+
+months = { January:  31,
+           February: 28,
+           March:    31,
+           April:    30,
+           May:      31,
+           June:     30,
+           July:     31,
+           August:   31,
+           September:30,
+           October:  31,
+           November: 30,
+           December: 31 }
+
+months.each { |m,d| puts m if d == 30 }
